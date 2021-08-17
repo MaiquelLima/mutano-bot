0329f034006459257543ce2877f45ef1e2f9e097
guilhermedsribeiro <guiguiribeiro.2020@gmail.com>
2021-08-16 23:29:19 -0300

    atualizando pressf

--- Commands/Fun/pressf.js
+++ Commands/Fun/pressf.js
@@ -4,14 +4,24 @@ module.exports = ({
   aliases: ["precionef", "pressionef", "f"],
   code: `
 $if[$getServerVar[lang]==pt]
-$reply[$messageID;{author: $userTag[$authorID]}{title: F}{description: Press F para $message}{thumbnail: $authorAvatar}{color: #ff0000};no]
-$addReactions[:regional_indicator_f:]
+$author[$userTag[$authorID]]
+$title[F]
+$description[Press F para $message]
+$thumbnail[$authorAvatar]
+$addTimestamp
+$color[#ff0000]
+$addReactions[<:Fortnite_F:875449376066117632>]
 $argsCheck[>1;{description: \`❌\`・Digite sua mensagem}{color: #ff0000}]
 $onlyIf[$getGlobalUserVar[bl;$authorID]==yes;**:x: » Você está na minha blacklist, não pode executar meus comandos!**]
 $cooldown[5s;🙆‍♀️・Vai com calma pequeno caçador. Espere %time% para executar meus comandos comandos]
 $elseif[$getServerVar[lang]==en]
-$reply[$messageID;{author: $userTag[$authorID]}{title: F}{description: Press F for $message}{thumbnail: $authorAvatar}{color: #ff0000};no]
-$addReactions[:regional_indicator_f:]
+$author[$userTag[$authorID]]
+$title[F]
+$description[Press F for $message]
+$thumbnail[$authorAvatar]
+$addTimestamp
+$color[#ff0000]
+$addReactions[<:Fortnite_F:875449376066117632>]
 $argsCheck[>1;{description: \`❌\`・Type your message}{color: #ff0000}]
 $onlyIf[$getGlobalUserVar[bl;$authorID]==yes;**:x: You are in my blacklist, so you can't execute my commands!**]
 $cooldown[5s;🙆‍♀️・Take it easy, little hunter. Wait %time% to execute my commands]
