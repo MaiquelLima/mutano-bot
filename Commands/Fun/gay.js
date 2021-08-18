441b72d50e4a1060b2021db7cd613432f166a3e3
guilhermedsribeiro <guiguiribeiro.2020@gmail.com>
2021-08-17 23:43:44 -0300

    comando duvidoso adicionado...

--- Commands/Fun/gay.js
+++ Commands/Fun/gay.js
@@ -4,11 +4,11 @@ module.exports = ({
     aliases: ["gaynometro", "gaynômetro"],
     code: `
 $if[$getServerVar[lang]==pt]
-$reply[$messageID;{description: :rainbow_flag: <@$findUser[$message]> você é \`$random[1;100]%\` gay :flushed:}{color: #ea93dc}{thumbnail: $authorAvatar}{author: $userTag[$authorID]};yes]
+$reply[$messageID;{description: :rainbow_flag: <@$findUser[$message]> você é \`$random[1;100]%\` gay :flushed:}{color: #ea93dc}{thumbnail: $userAvatar[$findUser[$message]]}{author: $userTag[$authorID]}{footer: Comando utilizado para brincadeiras nada que vá além do devido respeito};yes]
 $onlyIf[$getGlobalUserVar[bl;$authorID]==yes;**:x: » Você está na minha blacklist, não pode executar meus comandos!**]
 $cooldown[5s;🙆‍♀️・Vai com calma pequeno caçador. Espere %time% para executar meus comandos]
 $elseif[$getServerVar[lang]==en]
-$reply[$messageID;{description: :rainbow_flag: <@$findUser[$message]> you are \`$random[1;100]%\` gay :flushed:}{color: #ea93dc}{thumbnail: $authorAvatar}{author: $userTag[$authorID]};yes]
+$reply[$messageID;{description: :rainbow_flag: <@$findUser[$message]> you are \`$random[1;100]%\` gay :flushed:}{color: #ea93dc}{thumbnail: $userAvatar[$findUser[$message]]}{author: $userTag[$authorID]}{footer: Command used for jokes nothing that goes beyond due respect};yes]
 $onlyIf[$getGlobalUserVar[bl;$authorID]==yes;**:x: You are in my blacklist, so you can't execute my commands!**]
 $cooldown[5s;🙆‍♀️・Take it easy, little hunter. Wait %time% to execute my commands]
 $endelseif
