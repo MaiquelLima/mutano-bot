da4c8758c07c7acaaca181ada074363c28b79b56
guilhermedsribeiro <guiguiribeiro.2020@gmail.com>
2021-08-19 22:09:12 -0300

    teste

--- Commands/Fun/test.js
+++ Commands/Fun/test.js
@@ -1,6 +1,7 @@
 module.exports = ({
     name: "test",
     code: `
+    $description[bah]
     $awaitButtons[$get[id];$authorID;click;awaitclick; Stop,{description:Only $userTag Can Use This Button}{color:#ff0000},64;1]
 $let[id;$apiMessage[Click Me;;{actionRow:Click Me,2,1,click};;yes]]
 `
