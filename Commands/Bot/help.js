1940dd4ff7c0957fda45b7f4b97906f3ed679bb8
guilhermedsribeiro <guiguiribeiro.2020@gmail.com>
2021-08-21 15:47:54 -0300

    help atualizado...

--- Commands/Bot/help.js
+++ Commands/Bot/help.js
@@ -18,7 +18,7 @@ $description[\`\`\`Abaixo se encontram meus comandos e as descrições deles. Es
 \`say\` | \`8ball\` | \`jockey-pô\` | \`ship\` | \`piada\` | \`pressf\` | \`gay\` | \`gado\`
 
 \`🌄\` __**IMAGENS**__
-\`trash\` | \`drake\` | \`impostor\` | \`poof\` | \`stonks\` | \`triggered\` | \`trump\` | \`pixel\` | \`sepia\` | \`grey\`
+\`trash\` | \`drake\` | \`impostor\` | \`poof\` | \`stonks\` | \`triggered\` | \`trump\` | \`pixel\` | \`sepia\` | \`grey\` | \`blurple\`
 
 \`🔨\` __**CONFIGURÁVEIS**__
 \`set-anuncios\`
@@ -51,7 +51,7 @@ $description[\`\`\`You can check my commands and their descriptions right below.
 \`say\` | \`8ball\` | \`jockey-pô\` | \`ship\` | \`joke\` | \`pressf\` | \`gay\` | \`cattle\`
 
 \`🌄\` __**IMAGES**__
-\`trash\` | \`drake\` | \`imposter\` | \`poof\` | \`stonks\` | \`triggered\` | \`trump\` | \`pixelate\` | \`sepia\` | \`greyscale\`
+\`trash\` | \`drake\` | \`imposter\` | \`poof\` | \`stonks\` | \`triggered\` | \`trump\` | \`pixelate\` | \`sepia\` | \`greyscale\` | \`blurple\`
 
 \`🔨\` __**CONFIGURABLES**__
 \`set-announcements\`
