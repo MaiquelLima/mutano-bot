0645d0463542d0d8974b83126fb6ab52bb7660cf
guilhermedsribeiro <guiguiribeiro.2020@gmail.com>
2021-08-21 17:39:21 -0300

    corrigindo bugs...

--- Commands/Images/triggered.js
+++ Commands/Images/triggered.js
@@ -5,15 +5,15 @@ module.exports = ({
     code: `
 $if[$getServerVar[lang]==pt]
 <@$authorID>
-$image[https://some-random-api.ml/canvas/triggered?avatar=$replaceText[$replaceText[$replaceText[$replaceText[$replaceText[$userAvatar[$findUser[$message]];?size=2048;?size=4096];gif;png];webp;png];jpg;png];jpeg;png]]
+$image[https://some-random-api.ml/canvas/triggered?avatar=$replaceText[$replaceText[$replaceText[$replaceText[$userAvatar[$findUser[$message]?size=4096];gif;png];png;png];jpg;png];jpeg;png]]
 $footer[Solicitado por $username;$authorAvatar]
 $addTimestamp
 $color[#1ad46d]
 $onlyIf[$getGlobalUserVar[bl;$authorID]==yes;**:x: » Você está na minha blacklist, não pode executar meus comandos!**]
-$cooldown[5s;🙆‍♀️・Vai com calma pequeno caçador. Espere %time% para executar meus comandos]
+$cooldown[5s;🙆‍♀️・Vai com calma pequeno caçador. Espere %time% para executar meus comandos comandos]
 $elseif[$getServerVar[lang]==en]
 <@$authorID>
-$image[https://some-random-api.ml/canvas/triggered?avatar=$replaceText[$replaceText[$replaceText[$replaceText[$replaceText[$userAvatar[$findUser[$message]];?size=2048;?size=4096];gif;png];webp;png];jpg;png];jpeg;png]]
+$image[https://some-random-api.ml/canvas/triggered?avatar=$replaceText[$replaceText[$replaceText[$replaceText[$userAvatar[$findUser[$message]?size=4096];gif;png];png;png];jpg;png];jpeg;png]]
 $footer[Asked by $username;$authorAvatar]
 $addTimestamp
 $color[#1ad46d]
