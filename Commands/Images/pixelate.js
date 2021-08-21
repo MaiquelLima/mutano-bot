18e860364d362cae20230582dbb9bc93627372e2
guilhermedsribeiro <guiguiribeiro.2020@gmail.com>
2021-08-21 14:23:52 -0300

    atualizando comando de pixelate

--- Commands/Images/pixelate.js
+++ Commands/Images/pixelate.js
@@ -4,14 +4,14 @@ module.exports = ({
     aliases: ["pixelate", "px", "pixel"],
     code: `
 $if[$getServerVar[lang]==pt]
-$image[https://some-random-api.ml/canvas/pixelate?avatar=$replaceText[$replaceText[$replaceText[$replaceText[$userAvatar[$findUser[$message]?size=4096];gif;png];png;png];jpg;png];jpeg;png]]
+$image[https://some-random-api.ml/canvas/pixelate?avatar=$replaceText[$replaceText[$replaceText[$replaceText[$userAvatar[$findUser[$message]?size=4096];gif;png];webp;png];jpg;png];jpeg;png]]
 $footer[Solicitado por $username;$authorAvatar]
 $addTimestamp
 $color[#1ad46d]
 $onlyIf[$getGlobalUserVar[bl;$authorID]==yes;**:x: » Você está na minha blacklist, não pode executar meus comandos!**]
 $cooldown[5s;🙆‍♀️・Vai com calma pequeno caçador. Espere %time% para executar meus comandos comandos]
 $elseif[$getServerVar[lang]==en]
-$image[https://some-random-api.ml/canvas/pixelate?avatar=$replaceText[$replaceText[$replaceText[$replaceText[$userAvatar[$findUser[$message]?size=4096];gif;png];png;png];jpg;png];jpeg;png]]
+$image[https://some-random-api.ml/canvas/pixelate?avatar=$replaceText[$replaceText[$replaceText[$replaceText[$userAvatar[$findUser[$message]?size=4096];gif;png];webp;png;jpg;png];jpeg;png]]
 $footer[Asked by $username;$authorAvatar]
 $addTimestamp
 $color[#1ad46d]
