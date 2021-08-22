bc76d311e9d9cdde1e10b9e8a75fc0e344d587ae
guilhermedsribeiro <guiguiribeiro.2020@gmail.com>
2021-08-22 16:18:10 -0300

    userinfo em teste (ingles)

--- Commands/Utilities/userinfo.js
+++ Commands/Utilities/userinfo.js
@@ -4,7 +4,23 @@ module.exports = ({
     aliases: ["ui", "user"],
     code: `
 $if[$getServerVar[lang]==pt]
+$thumbnail[$userAvatar[$findUser[$message]]]
+$author[$username[$findUser[$message]]]
+$description[・Account created at: \`$replaceText[$replaceText[$replaceText[$replaceText[$replaceText[$replaceText[$replaceText[$replaceText[$replaceText[$replaceText[$replaceText[$replaceText[$replaceText[$replaceText[$creationDate[$findUser[$message];time];months;meses];weeks;semanas];days;dias];hours;horas];minutes;minutos];and;e];seconds;segundos];second;segundo];minute;minuto];year;ano];years;anos];week;semana];day;dia];month;mês]\`
+
+・Joined here at: \`$replaceText[$replaceText[$replaceText[$replaceText[$replaceText[$replaceText[$replaceText[$replaceText[$replaceText[$replaceText[$replaceText[$replaceText[$replaceText[$replaceText[$memberJoinedDate[$findUser[$message];time];months;meses];weeks;semanas];days;dias];hours;horas];minutes;minutos];and;e];seconds;segundos];second;segundo];minute;minuto];year;ano];years;anos];week;semana];day;dia];month;mês]\`
+
+・He was the **$replaceText[$replaceText[$replaceText[$replaceText[$memberJoinPosition[$findUser[$message]];st; ];nd; ];rd; ];th; ]**to enter here
+
+$addField[・Discord Tag;\`$userTag[$findUser[$message]]\`;yes]
+$addField[・ID;\`$findUser[$message]\`;yes]
+$color[#1ad46d]
+$footer[Asked by $username[$authorID];$authorAvatar]
+
+$onlyIf[$getGlobalUserVar[bl;$authorID]==yes;**:x: » Você está na minha blacklist, não pode executar meus comandos!**]
+$cooldown[5s;🙆‍♀️・Vai com calma pequeno caçador. Espere %time% para executar meus comandos]
 $elseif[$getServerVar[lang]==en]
+
 $thumbnail[$userAvatar[$findUser[$message]]]
 $author[$username[$findUser[$message]]]
 $description[・Account created at: \`$replaceText[$replaceText[$replaceText[$replaceText[$replaceText[$replaceText[$replaceText[$replaceText[$replaceText[$replaceText[$replaceText[$replaceText[$replaceText[$replaceText[$creationDate[$findUser[$message];time];months;meses];weeks;semanas];days;dias];hours;horas];minutes;minutos];and;e];seconds;segundos];second;segundo];minute;minuto];year;ano];years;anos];week;semana];day;dia];month;mês]\`
