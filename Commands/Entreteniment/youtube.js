2bd91a9be257baec75901a66d6c59628a12c677d
guilhermedsribeiro <guiguiribeiro.2020@gmail.com>
2021-08-24 13:34:30 -0300

    atualizando comando de youtube...

--- Commands/Entreteniment/youtube.js
+++ Commands/Entreteniment/youtube.js
@@ -7,13 +7,10 @@ module.exports = ({
     
     client.discordTogether = new DiscordTogether(client);
     client.discordTogether.createTogetherCode('$voiceID', 'youtube').then(async invite => {
-     return message.channel.send(":youtube:・**Aqui está o seu convite para assistir o YouTube:** " + invite.code)
+     return message.channel.send("<:youtube:879760132916007002>・**Aqui está o seu convite para assistir o YouTube:** " + invite.code)
      });]
-    $else
-    Não encontrei nada
-    $endif
     $onlyIf[$voiceID!=;\:x:・Você não está conectado em um canal de voz]
     $onlyIf[$hasPerms[$authorID;speak]==true;\:x:・Você não tem permissão para falar]
-    $onlyIf[$getServerVar[lang]==en;]
+    $onlyIf[$getServerVar[lang]==pt;]
     `
 })
--- Commands/Entreteniment/youtube2.js
+++ Commands/Entreteniment/youtube2.js
@@ -7,13 +7,10 @@ module.exports = ({
     
     client.discordTogether = new DiscordTogether(client);
     client.discordTogether.createTogetherCode('$voiceID', 'youtube').then(async invite => {
-     return message.channel.send(":youtube:・**Here is your invitation to watch YouTube:** " + invite.code)
+     return message.channel.send("<:youtube:879760132916007002>・**Here is your invitation to watch YouTube:** " + invite.code)
      });]
-    $else
-    I found nothing
-    $endif
     $onlyIf[$voiceID!=;\:x:・You are not connected to a voice channel]
     $onlyIf[$hasPerms[$authorID;speak]==true;\:x:・You are not allowed to speak]
-    $onlyIf[$getServerVar[lang]==pt;]
+    $onlyIf[$getServerVar[lang]==en;]
     `
 })
