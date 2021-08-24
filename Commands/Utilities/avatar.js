25bd0647fcb73cc1119b87c08ff992b337af5a81
guilhermedsribeiro <guiguiribeiro.2020@gmail.com>
2021-08-24 12:16:08 -0300

    atualizando o avatar...

--- Commands/Utilities/avatar.js
+++ Commands/Utilities/avatar.js
@@ -5,30 +5,22 @@ module.exports = ({
     code: `
   $if[$getServerVar[lang]==pt]
   
-  $author[Avatar de $username[$findUser[$message;yes]]]
-  
-  $description[<@$findUser[$message;yes]> Que foto zika meu amigo!  Muito bom gosto :3!
-  
-  __Downloads__
-  [webp\\]($replaceText[$replaceText[$replaceText[$replaceText[$replaceText[$userAvatar[$findUser[$message]];gif;webp];png;webp];jpg;webp];jpeg;webp];?size=2048;?size=4096]) | [png\\]($replaceText[$replaceText[$replaceText[$replaceText[$replaceText[$userAvatar[$findUser[$message]];gif;png];jpg;png];jpeg;png];webp;png];?size=2048;?size=4096]) | [jpg\\]($replaceText[$replaceText[$replaceText[$replaceText[$replaceText[$userAvatar[$findUser[$message]];gif;jpg];png;jpg];jpeg;jpg];webp;jpg];?size=2048;?size=4096]) | [jpeg\\]($replaceText[$replaceText[$replaceText[$replaceText[$replaceText[$replaceText[$userAvatar[$findUser[$message]];gif;jpeg];png;jpeg];jpg;jpeg];jpeg;jpeg];webp;jpeg];?size=2048;?size=4096]) | [gif\\]($replaceText[$replaceText[$replaceText[$replaceText[$replaceText[$replaceText[$userAvatar[$findUser[$message]];png;gif];jpg;gif];jpeg;gif];gif;gif];webp;gif];?size=2048;?size=4096])
-  $image[$replaceText[$replaceText[$userAvatar[$findUser[$message]];?size=2048;?size=4096];width=480&height=480;?size=4096]
-  
-  $footer[Pedido por $username]
-  $color[#1ad46d]
+  $apiMessage[$channelID;;{footer: Executed by $userTag}{title:Avatar de $username[$findUser[$message;yes]]}{image:$useravatar[$finduser[$message]]}{description:<@$findUser[$message;yes]> Que foto zika meu amigo!  Muito bom gosto :3!
+
+    __Downloads nos Botões:__}$addTimestamp{color:#1ad46d;{actionRow:WEBP,2,5,$replaceText[$replaceText[$replaceText[$replaceText[$replaceText[$replaceText[$userAvatar[$findUser[$message]];gif;webp];png;webp];jpg;webp];jpeg;webp];?size=2048;?size=4096];:;#COLON#]}{actionRow:PNG,2,5,$replaceText[$replaceText[$replaceText[$replaceText[$replaceText[$replaceText[$userAvatar[$findUser[$message]];gif;png];jpg;png];jpeg;png];webp;png];?size=2048;?size=4096];:;#COLON#]}
+    {actionRow:JPG,2,5,$replaceText[$replaceText[$replaceText[$replaceText[$replaceText[$replaceText[$userAvatar[$findUser[$message]];gif;jpg];png;jpg];jpeg;jpg];webp;jpg];?size=2048;?size=4096];:;#COLON#]}
+    {actionRow:JPEG,2,5,$replaceText[$replaceText[$replaceText[$replaceText[$replaceText[$replaceText[$replaceText[$userAvatar[$findUser[$message]];gif;jpeg];png;jpeg];jpg;jpeg];jpeg;jpeg];webp;jpeg];?size=2048;?size=4096];:;#COLON#]}
+    {actionRow:GIF,2,5,$replaceText[$replaceText[$replaceText[$replaceText[$replaceText[$replaceText[$replaceText[$userAvatar[$findUser[$message]];png;gif];jpg;gif];jpeg;gif];gif;gif];webp;gif];?size=2048;?size=4096];:;#COLON#]};;no]
   $cooldown[5s;🙆‍♀️・Vai com calma pequeno caçador. Espere %time% para executar meus comandos denovo!]
   $onlyIf[$getGlobalUserVar[bl;$authorID]==yes;** :x: » Você está na minha blacklist, não pode executar meus comandos!**]
   $elseif[$getServerVar[lang]==en]
   
-  $author[$username[$findUser[$message;yes]]'s Avatar]
-  $description[
-  <@$findUser[$message;yes]> What an epic picture!  You have really good taste :3!
-  
-  __Downloads__
-  [webp\\]($replaceText[$replaceText[$replaceText[$replaceText[$replaceText[$userAvatar[$findUser[$message]];gif;webp];png;webp];jpg;webp];jpeg;webp];?size=2048;?size=4096]) | [png\\]($replaceText[$replaceText[$replaceText[$replaceText[$replaceText[$userAvatar[$findUser[$message]];gif;png];jpg;png];jpeg;png];webp;png];?size=2048;?size=4096]) | [jpg\\]($replaceText[$replaceText[$replaceText[$replaceText[$replaceText[$userAvatar[$findUser[$message]];gif;jpg];png;jpg];jpeg;jpg];webp;jpg];?size=2048;?size=4096]) | [jpeg\\]($replaceText[$replaceText[$replaceText[$replaceText[$replaceText[$replaceText[$userAvatar[$findUser[$message]];gif;jpeg];png;jpeg];jpg;jpeg];jpeg;jpeg];webp;jpeg];?size=2048;?size=4096]) | [gif\\]($replaceText[$replaceText[$replaceText[$replaceText[$replaceText[$replaceText[$userAvatar[$findUser[$message]];png;gif];jpg;gif];jpeg;gif];gif;gif];webp;gif];?size=2048;?size=4096])
-  $image[$replaceText[$replaceText[$userAvatar[$findUser[$message]];?size=2048;?size=4096];width=480&height=480;?size=4096]
-  
-  $footer[Executed by: $username]
-  $color[#1ad46d]
+$apiMessage[$channelID;;{footer: Executed by $userTag}{title:$username[$findUser[$message;yes]]'s Avatar}{image:$useravatar[$finduser[$message]]}{description:<@$findUser[$message;yes]> What an epic picture!  You have really good taste :3!
+
+    __Downloads in Buttons:__}$addTimestamp{color:#1ad46d;{actionRow:WEBP,2,5,$replaceText[$replaceText[$replaceText[$replaceText[$replaceText[$replaceText[$userAvatar[$findUser[$message]];gif;webp];png;webp];jpg;webp];jpeg;webp];?size=2048;?size=4096];:;#COLON#]}{actionRow:PNG,2,5,$replaceText[$replaceText[$replaceText[$replaceText[$replaceText[$replaceText[$userAvatar[$findUser[$message]];gif;png];jpg;png];jpeg;png];webp;png];?size=2048;?size=4096];:;#COLON#]}
+    {actionRow:JPG,2,5,$replaceText[$replaceText[$replaceText[$replaceText[$replaceText[$replaceText[$userAvatar[$findUser[$message]];gif;jpg];png;jpg];jpeg;jpg];webp;jpg];?size=2048;?size=4096];:;#COLON#]}
+    {actionRow:JPEG,2,5,$replaceText[$replaceText[$replaceText[$replaceText[$replaceText[$replaceText[$replaceText[$userAvatar[$findUser[$message]];gif;jpeg];png;jpeg];jpg;jpeg];jpeg;jpeg];webp;jpeg];?size=2048;?size=4096];:;#COLON#]}
+    {actionRow:GIF,2,5,$replaceText[$replaceText[$replaceText[$replaceText[$replaceText[$replaceText[$replaceText[$userAvatar[$findUser[$message]];png;gif];jpg;gif];jpeg;gif];gif;gif];webp;gif];?size=2048;?size=4096];:;#COLON#]};;no]
   $cooldown[5s;🙆‍♀️・Take it easy ,little hunter. wait %time% to execute my commands again!]
   $onlyIf[$getGlobalUserVar[bl;$authorID]==yes;** :x: » You are in my blacklist, you can't execute my commands!**]
   $endelseif
