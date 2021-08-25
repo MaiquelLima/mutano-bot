a9f884ce047e8b51d5a9e09a586eac11bfa64f08
guilhermedsribeiro <guiguiribeiro.2020@gmail.com>
2021-08-25 07:56:24 -0300

    tirando add reaction do ping

--- Commands/Bot/ping.js
+++ Commands/Bot/ping.js
@@ -4,13 +4,11 @@ module.exports = ({
     aliases: ["ms","latency","latencia","latência"],
     code: `
 $if[$getServerVar[lang]==pt]
-$addCmdReactions[🏓]
 🐯 Minha Latência é: \`$botPingms\`
 ⚡ Latência da WebSocket é: \`$djsEval[client.ws.ping;yes]ms\`
 $onlyIf[$getGlobalUserVar[bl;$authorID]==yes;**:x: » Você está na minha blacklist, não pode executar meus comandos!**]
 $cooldown[5s;🙆‍♀️・Vai com calma pequeno caçador. Espere %time% para executar meus comandos]
 $elseif[$getServerVar[lang]==en]
-$addCmdReactions[🏓]
 🐯 My Latency is: \`$botPingms\` 
 ⚡ WebSocket Latency is: \`$djsEval[client.ws.ping;yes]ms\`
 $onlyIf[$getGlobalUserVar[bl;$authorID]==yes;**:x: You are in my blacklist, so you can't execute my commands!**]
