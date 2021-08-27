96f7cfcaea018cea09fd70f2bd86739b1618233e
guilhermedsribeiro <guiguiribeiro.2020@gmail.com>
2021-08-27 14:12:51 -0300

    servericon temporáriamente removido..

--- Commands/Utilities/servericon.js
+++
@@ -1,8 +0,0 @@
-module.exports = ({
-    name: "servericon",
-    description: "Use isto para ver o ícone de algum servidor...",
-    aliases: ["svicon", "iconserver", "icon-server", "iconsv", "icon-sv"],
-    code: `
-    
-    `
-})
