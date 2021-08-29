e441f080d4fd8c282e5914715cb7dde47fb498b7
guih rib <guiguiribeiro.2020@gmail.com>
2021-08-29 09:43:56 -0300

    botinfo atualizado...

--- Commands/Bot/botinfo.js
+++ Commands/Bot/botinfo.js
@@ -19,6 +19,7 @@ __**Criado em:**__
 
 __**CRIADOR**__
 \`$username[852616816240885760]#$discriminator[852616816240885760]\` | \`852616816240885760\` | [GitHub - guihrib](https://github.com/guihrib)}{footer:Solicitado por $username}{color:#1ad46d};{actionRow:WebSite Oficial,2,5,$replaceText[https://mutanobot.site/;:;#COLON#];;no]
+$botTyping
 $onlyIf[$getGlobalUserVar[bl;$authorID]==yes;**:x: » Você está na minha blacklist, não pode executar meus comandos!**]
 $cooldown[5s;️🙆‍♀️・Vai com calma pequeno caçador. Espere %time% para executar meus comandos]
 $elseif[$getServerVar[lang]==en]
@@ -37,6 +38,7 @@ __**Created in:**__
 
 __**CREATOR**__
 \`$username[852616816240885760]#$discriminator[852616816240885760]\` | \`852616816240885760\` | [GitHub - guihrib](https://github.com/guihrib)}{footer:Asked by $username}{color:#1ad46d};{actionRow:Oficial WebSite,2,5,$replaceText[https://mutanobot.site/;:;#COLON#];;no]
+$botTyping
 $onlyIf[$getGlobalUserVar[bl;$authorID]==yes;**:x: You are in my blacklist, so you can't execute my commands!**]
 $cooldown[5s;️🙆‍♀️・Take it easy, little hunter. Wait %time% to execute my commands]
 $endelseif
