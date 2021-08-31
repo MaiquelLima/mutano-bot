c1b61c983647517427bdc20a6c08b22c1716837a
guilhermedsribeiro <guiguiribeiro.2020@gmail.com>
2021-08-31 00:07:56 -0300

    wink e eval atualizados...

--- Commands/Devs/eval.js
+++ Commands/Devs/eval.js
@@ -22,7 +22,7 @@ $eval[$message;yes]
 \`\`\`]
 $suppressErrors
 $argsCheck[>1;{description: Digite algo para inspecionar}{color: #ff0000}]
-$onlyForIDs[852616816240885760;857836846641578014;817323606257565736;{description: Apenas meus Desenvolvedores podem executar este comando!}{color: #ff0000}]
+$onlyForIDs[852616816240885760;857836846641578014;683728874390945823;711969991427227729;817323606257565736;{description: Apenas meus Desenvolvedores podem executar este comando!}{color: #ff0000}]
 $elseif[$getServerVar[lang]==en]
 
 $color[#1ad46d]
@@ -42,7 +42,7 @@ $eval[$message;yes]
 \`\`\`]
 $suppressErrors
 $argsCheck[>1;{description: Type something to inspect}{color: #ff0000}]
-$onlyForIDs[852616816240885760;857836846641578014;817323606257565736;{description: <a:Check_No:810066692842586142> » Only my Developers can run this command!}{color: #ff0000}]
+$onlyForIDs[852616816240885760;857836846641578014;683728874390945823;711969991427227729;817323606257565736;{description: <a:Check_No:810066692842586142> » Only my Developers can run this command!}{color: #ff0000}]
 $endelseif
 $endif
     `
--- Commands/Entreteniment/wink.js
+++ Commands/Entreteniment/wink.js
@@ -5,24 +5,26 @@ module.exports = ({
     code: `
 $if[$getServerVar[lang]==pt]
 $author[$userTag[$authorID];$authorAvatar]
+$thumbnail[$userAvatar[$findUser[$message[1]]]
 $description[🥰 <@$findUser[$message]> recebeu uma piscadinha de <@$authorID> 😲]
 $image[$jsonRequest[https://some-random-api.ml/animu/wink;link;error]]
 $footer[Pedido por $username]
 $addTimestamp
 $color[#1ad46d]
 $onlyIf[$memberExists[$findUser[$message[1]]]==true;{description: \❌・Este usuário não existe no meu banco de dados}{color: #ff0000}]
-$argsCheck[>1;{description: \❌・Mencione ou insira o ID do usuário pra beijar}{color: #ff0000}]
+$argsCheck[>1;{description: \❌・Mencione ou insira o ID do usuário pra piscar}{color: #ff0000}]
 $onlyIf[$getGlobalUserVar[bl;$authorID]==yes;**:x: » Você está na minha blacklist, não pode executar meus comandos!**]
 $cooldown[5s;🙆‍♀️・Vai com calma pequeno caçador. Espere %time% para executar meus comandos]
 $elseif[$getServerVar[lang]==en]
 $author[$userTag[$authorID];$authorAvatar]
+$thumbnail[$userAvatar[$findUser[$message[1]]]
 $description[🥰 <@$findUser[$message]> received a wink from <@$authorID> 😲]
 $image[$jsonRequest[https://some-random-api.ml/animu/wink;link;error]]
 $footer[Asked by $username]
 $addTimestamp
 $color[#1ad46d]
 $onlyIf[$memberExists[$findUser[$message[1]]]==true;{description: \❌・This user does not exist in my database}{color: #ff0000}]
-$argsCheck[>1;{description: \❌・Mention or enter user ID to kiss}{color: #ff0000}]
+$argsCheck[>1;{description: \❌・Mention or enter user ID to wink}{color: #ff0000}]
 $onlyIf[$getGlobalUserVar[bl;$authorID]==yes;**:x: You are in my blacklist, so you can't execute my commands!**]
 $cooldown[5s;🙆‍♀️・Take it easy, little hunter. Wait %time% to execute my commands]
 $endelseif
