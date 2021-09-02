c3230e0cd8c4d164deccde60e3ab298adb588540
guilhermedsribeiro <guiguiribeiro.2020@gmail.com>
2021-09-02 19:02:53 -0300

    primeira atualização do cmd de equipe

--- Commands/Bot/team.js
+++ Commands/Bot/team.js
@@ -6,7 +6,15 @@ module.exports = ({
 $if[$getServerVar[lang]==pt]
 $author[Minha Equipe;$authorAvatar]
 $thumbnail[$userAvatar[$clientID]]
-$description[]
+$description[\`\`\`
+Aqui você irá encontrar uma descrição mais precisa de toda a minha equipe, os que me desenvolvem, suas funções. Os que gerenciam minha parte empreserial, divulgações e etc... Tudo isso, e muito mais abaixo :3
+\`\`\`]
+$onlyIf[$getGlobalUserVar[bl;$authorID]==yes;**:x: » Você está na minha blacklist, não pode executar meus comandos!**]
+$cooldown[5s;🙆‍♀️・Vai com calma pequeno caçador. Espere %time% para executar meus comandos]
+$elseif[$getServerVar[lang]==en]
+$onlyIf[$getGlobalUserVar[bl;$authorID]==yes;**:x: You are in my blacklist, so you can't execute my commands!**]
+$cooldown[5s;🙆‍♀️・Take it easy, little hunter. Wait %time% to execute my commands]
+$endelseif
 $endif
     `
 })
