ca36973c1195db7adc007c4224383b2022a81f18
guihrib <guiguiribeiro.2020@gmail.com>
2021-09-06 00:08:55 -0300

    work....

--- Commands/Economy/work.js
+++ Commands/Economy/work.js
@@ -9,13 +9,15 @@ module.exports = ({
   $author[$userTag[$authorID];$authorAvatar]
   $description[$randomText[Você foi na **Floresta Amazônica** e vendou uma pele de onça que caçou. Com isso ganhou #CHAR#TC\`$random[1100;3600]\` TigerCoins]]
   $footer[Mutano™・Economia;$userAvatar[$clientID]]
+  
+$else[$getGlobalUserVar[emprego;$authorID]==nenhum;{description: Você está desempregado
+> Use \`$getServerVar[chamada]empregos\` para ver a lista de empregos}{color: #ff0000}]
 
-$else[$getGlobalUserVar[emprego;$authorID]==Programador;]
+$elseif[$getGlobalUserVar[emprego;$authorID]==Programador;]
 $author[$userTag[$authorID];$authorAvatar]
 $description[$randomText[Você foi na **Floresta Amazônica** e vendou uma pele de onça que caçou. Com isso ganhou #CHAR#TC\`$random[1100;3600]\` TigerCoins]] 
   $endif
+  $endelse
   $endelseif
-  $onlyIf[$getGlobalUserVar[emprego;$authorID]==nenhum;{description: Você está desempregado
-> Use \`$getServerVar[chamada]empregos\` para ver a lista de empregos}{color: #ff0000}]
 `
 })
