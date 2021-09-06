57e62738b3a0288799c3e4017f6290ca907db3f6
Bayziofc <72474595+Bayziofc@users.noreply.github.com>
2021-09-06 11:34:51 -0300

    Update serverinfo.js

--- Commands/Utilities/serverinfo.js
+++ Commands/Utilities/serverinfo.js
@@ -3,7 +3,7 @@ module.exports = ({
     description: "Principais informações do servidor",
     aliases: ["infoserver","info-server","server-info"],
     code: `
-$if[$getServerVar[lang]==pt]
+
 $author[$serverName;$serverIcon]
 $thumbnail[$serverIcon]
 $description[\`\`\`Abaixo se encontra as informações sobre o servidor\`\`\`
@@ -35,39 +35,8 @@ $addTimestamp
 $color[#1ad46d]
 $onlyIf[$getGlobalUserVar[bl;$authorID]==yes;**:x: » Você está na minha blacklist, não pode executar meus comandos!**]
 $cooldown[5s;🙆‍♀️・Vai com calma pequeno caçador. Espere %time% para executar meus comandos]
-$elseif[$getServerVar[lang]==en]
-$author[$serverName;$serverIcon]
-$thumbnail[$serverIcon]
-$description[\`\`\` You can check the information about the server here:\`\`\`
 
-__**Important**__
-\`👑\` Owner: <@$ownerID> | \`$ownerID\`
-ID server: \`$guildID\`
-Created in: \`$creationDate[$guildID]\` | ($creationDate[$guildID;time])
 
-__**Dates**__
-Region: \`$serverRegion\`
-Emojis: \`$emojiCount\`
-Roles: \`$roleCount\`
-Highest Role \`$roleName[$highestServerRole]\`
-
-__**Channels:{$channelCount}**__
-Text: \`$channelCount[text]\`
-Voice: \`$channelCount[voice]\`
-
-__**Members: ($membersCount)**__
-
-__**Nitro**__
-\`🚀\` Boosts: \`$serverBoostCount\`
-\`🌎\` Boost Level: \`Lvl: $serverBoostLevel\`]
-$image[$replaceText[$replaceText[$serverBanner;webp;png;gif]?size=4096;null;https://cdn.discordapp.com/attachments/858905592801656853/867814690482028564/banner_invisible.png;]]
-$footer[Executed by: $username;$authorAvatar] 
-$addTimestamp
-$color[#1ad46d]
-$onlyIf[$getGlobalUserVar[bl;$authorID]==yes;**:x: You are in my blacklist, so you can't execute my commands!**]
-$cooldown[5s;🙆‍♀️・Take it easy, little hunter. Wait %time% to execute my commands]
-$endelseif
-$endif
 
     `
 })
