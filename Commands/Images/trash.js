8dd727f321f38c61c118c346b64b2c03d82d1f07
VoidZin <58865038+VoidZin14@users.noreply.github.com>
2021-09-06 13:16:15 -0300

    Update trash.js

--- Commands/Images/trash.js
+++ Commands/Images/trash.js
@@ -3,7 +3,6 @@ module.exports = {
     description: "Opa... É um User? Ou um lixo? Hmmm...",
 	aliases: ['lixo', 'lixeiro'],
 	code: `
-$if[$getServerVar[lang]==pt]
 <@$authorID>
 $image[https://api.cool-img-api.ml/trash?image=$replaceText[$replaceText[$replaceText[$userAvatar[$findUser[$message]];?size=2048;?size=4096];webp;png];gif;png]
 $footer[Solicitado por $username;$authorAvatar]
@@ -11,15 +10,5 @@ $addTimestamp
 $color[#1ad46d]
 $onlyIf[$getGlobalUserVar[bl;$authorID]==yes;**:x: » Você está na minha blacklist, não pode executar meus comandos!**]
 $cooldown[5s;🙆‍♀️・Vai com calma pequeno caçador. Espere %time% para executar meus comandos]
-$elseif[$getServerVar[lang]==en]
-<@$authorID>
-$image[https://api.cool-img-api.ml/trash?image=$replaceText[$replaceText[$replaceText[$userAvatar[$findUser[$message]];?size=2048;?size=4096];webp;png];gif;png]
-$footer[Asked by $username;$authorAvatar]
-$addTimestamp
-$color[#1ad46d]
-$onlyIf[$getGlobalUserVar[bl;$authorID]==yes;**:x: You are in my blacklist, so you can't execute my commands!**]
-$cooldown[5s;🙆‍♀️・Take it easy, little hunter. Wait %time% to execute my commands]
-$endelseif
-$endif
     `
 };
