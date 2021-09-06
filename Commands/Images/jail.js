77cc31fc3e77c57fc171995c7ab0463511d46688
VoidZin <58865038+VoidZin14@users.noreply.github.com>
2021-09-06 13:13:41 -0300

    Update jail.js

--- Commands/Images/jail.js
+++ Commands/Images/jail.js
@@ -3,21 +3,12 @@ module.exports = ({
     description: "Infelizmente todos neste comando, acabão em jaulas 😔",
     aliases: ["jaula", "preso", "aprisionado", "enjaulado", "prisão", "prisao", "cadeia"],
     code: `
-$if[$getServerVar[lang]==pt]
+
 $image[https://some-random-api.ml/canvas/jail?avatar=$replaceText[$replaceText[$replaceText[$userAvatar[$findUser[$message]];?size=2048;?size=4096];webp;png];gif;png]
 $footer[Solicitado por $username;$authorAvatar]
 $addTimestamp
 $color[#1ad46d]
 $onlyIf[$getGlobalUserVar[bl;$authorID]==yes;**:x: » Você está na minha blacklist, não pode executar meus comandos!**]
 $cooldown[5s;🙆‍♀️・Vai com calma pequeno caçador. Espere %time% para executar meus comandos]
-$elseif[$getServerVar[lang]==en]
-$image[https://some-random-api.ml/canvas/jail?avatar=$replaceText[$replaceText[$replaceText[$userAvatar[$findUser[$message]];?size=2048;?size=4096];webp;png];gif;png]
-$footer[Asked by $username;$authorAvatar]
-$addTimestamp
-$color[#1ad46d]
-$onlyIf[$getGlobalUserVar[bl;$authorID]==yes;**:x: You are in my blacklist, so you can't execute my commands!**]
-$cooldown[5s;🙆‍♀️・Take it easy, little hunter. Wait %time% to execute my commands]
-$endelseif
-$endif
     `
 })
